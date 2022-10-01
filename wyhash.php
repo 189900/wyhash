a8f741825f5929650a31f805d078125ac5465400
none <189900@gmail.com>
2022-10-01 15:29:10 +0300

    hasher

--- wyhash.php
+++ wyhash.php
@@ -6,19 +6,17 @@ namespace n189900\Wyhash;
 
 use GMP;
 
-class Hasher
+class State
 {
     public const UPDATE_SIZE = 64;
-    public const ROUND_SIZE = 48;
+    private const ROUND_SIZE = 48;
 
     private static array $primes = [];
 
     private GMP $seed;
-    private int $length = 0;
-
     private GMP $one;
     private GMP $two;
-
+    private int $length = 0;
     private string $tail = '';
 
     public function __construct(string $seed = '0')
@@ -35,69 +33,6 @@ class Hasher
         $this->seed = gmp_init($seed) ^ self::$primes[0];
         $this->one = clone $this->seed;
         $this->two = clone $this->seed;
-    }
-
-    public static function hash(string $buffer, string $seed = '0'): string
-    {
-        $wh = new self($seed);
-        $length = mb_strlen($buffer, '8bit');
-
-        $a = gmp_init(0);
-        $b = gmp_init(0);
-        if ($length <= 16) {
-            if ($length >= 4) {
-                $a = $wh->readBytes(4, $buffer) << 32
-                    | $wh->readBytes(4, $buffer, ($length >> 3) << 2);
-                $b = $wh->readBytes(4, $buffer, $length - 4) << 32
-                    | $wh->readBytes(4, $buffer, $length - 4 - (($length >> 3) << 2));
-            } elseif ($length > 0) {
-                $a = $wh->wyr3($buffer, $length);
-            }
-        } else {
-            $i = $length;
-            $offset = 0;
-            if ($i > 48) {
-                $see1 = clone $wh->seed;
-                $see2 = clone $wh->seed;
-                do {
-                    $wh->seed = $wh->mix(
-                        $wh->readBytes(8, $buffer, $offset) ^ self::$primes[1],
-                        $wh->readBytes(8, $buffer, $offset + 8) ^ $wh->seed,
-                    );
-                    $see1 = $wh->mix(
-                        $wh->readBytes(8, $buffer, $offset + 16) ^ self::$primes[2],
-                        $wh->readBytes(8, $buffer, $offset + 24) ^ $see1,
-                    );
-                    $see2 = $wh->mix(
-                        $wh->readBytes(8, $buffer, $offset + 32) ^ self::$primes[3],
-                        $wh->readBytes(8, $buffer, $offset + 40) ^ $see2,
-                    );
-                    $i -= 48;
-                    $offset += 48;
-                } while ($i > 48);
-                $wh->seed ^= $see1 ^ $see2;
-            }
-            while ($i > 16) {
-                $wh->seed = $wh->mix(
-                    $wh->readBytes(8, $buffer, $offset) ^ self::$primes[1],
-                    $wh->readBytes(8, $buffer, $offset + 8) ^ $wh->seed,
-                );
-                $i -= 16;
-                $offset += 16;
-            }
-            $a = $wh->readBytes(8, $buffer, $offset + $i - 16);
-            $b = $wh->readBytes(8, $buffer, $offset + $i - 8);
-        }
-
-        $result = $wh->mix(
-            self::$primes[1] ^ $length,
-            $wh->mix(
-                $a ^ self::$primes[1],
-                $b ^ $wh->seed,
-            ),
-        );
-
-        return str_pad(gmp_strval($result, 16), 16, '0', STR_PAD_LEFT);
     }
 
     /**
@@ -179,7 +114,7 @@ class Hasher
     }
 
     /**
-     * Accepts a 48-byte aligned buffer.
+     * Handles a 48-byte aligned buffer. Only used if payload length is greater than ROUND_SIZE.
      */
     private function round(string $buffer): void
     {
@@ -204,12 +139,12 @@ class Hasher
         $this->length += $length;
     }
 
-    private function readBytes(int $bytes, string $buffer, int $offset = 0): GMP
+    private function readBytes(int $bytes, string &$buffer, int $offset = 0): GMP
     {
         return gmp_import(substr($buffer, $offset, $bytes), $bytes, GMP_LITTLE_ENDIAN);
     }
 
-    private function wyr3(string $buffer, int $length): GMP
+    private function wyr3(string &$buffer, int $length): GMP
     {
         return $this->readBytes(1, $buffer) << 16
             | $this->readBytes(1, $buffer, $length >> 1) << 8
@@ -231,6 +166,45 @@ class Hasher
     }
 }
 
+class Hasher
+{
+    private State $state;
+    private string $tail = '';
+
+    public function __construct(string $seed = '0')
+    {
+        $this->state = new State($seed);
+    }
+
+    public static function hash(string $buffer, string $seed = '0'): string
+    {
+        return (new self($seed))->final($buffer);
+    }
+
+    public function update(string $buffer): self
+    {
+        $this->tail .= $buffer;
+        $length = mb_strlen($this->tail, '8bit');
+
+        if ($aligned = $length - ($length % State::UPDATE_SIZE)) {
+            $this->state->update(substr($this->tail, 0, $aligned));
+            $this->tail = (string) substr($this->tail, $aligned);
+        }
+
+        return $this;
+    }
+
+    public function final(string $buffer = ''): string
+    {
+        if (isset($buffer[State::UPDATE_SIZE - 1])) {
+            $this->update($buffer);
+            $buffer = '';
+        }
+
+        return $this->state->final($this->tail . $buffer);
+    }
+}
+
 (ini_get('zend.assertions') === '1' && ini_get('assert.active') === '1') || die('assertions disabled');
 
 foreach ([
@@ -242,16 +216,9 @@ foreach ([
     ['ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', '9199383239c32554'],
     ['12345678901234567890123456789012345678901234567890123456789012345678901234567890', '7c1ccf6bba30f5a5'],
 ] as $seed => [$msg, $hash]) {
+    // echo $msg . '=' . ($eh = State::hash($msg, (string) $seed)) . "\n";
+    // assert($eh === $hash);
     echo $msg . '=' . ($eh = Hasher::hash($msg, (string) $seed)) . "\n";
-    assert($eh === $hash);
-
-    $h = new Hasher((string) $seed);
-    $chunks = str_split($msg, 64);
-    $tail = array_pop($chunks);
-    foreach ($chunks as $chunk) {
-        $h->update($chunk);
-    }
-    echo $msg . '=' . ($eh = $h->final($tail)) . "\n";
     assert($eh === $hash);
 }
 
@@ -287,16 +254,9 @@ foreach ([
     ['abcdefghijklmnopqrstuvwxyz1234567890123456789012', '0x1b', 'b43f94f2a56f15aa'],
     ['abcdefghijklmnopqrstuvwxyz12345678901234567890123', '0x1c', '7feb635da5d73429'],
 ] as [$msg, $seed, $hash]) {
-    echo $msg . '=' . ($eh = Hasher::hash($msg, $seed)) . "\n";
-    assert($eh === $hash);
-
-    $h = new Hasher((string) $seed);
-    $chunks = str_split($msg, 64);
-    $tail = array_pop($chunks);
-    foreach ($chunks as $chunk) {
-        $h->update($chunk);
-    }
-    echo $msg . '=' . ($eh = $h->final($tail)) . "\n";
+    // echo $msg . '=' . ($eh = State::hash($msg, $seed)) . "\n";
+    // assert($eh === $hash);
+    echo $msg . '=' . ($eh = Hasher::hash($msg, (string) $seed)) . "\n";
     assert($eh === $hash);
 }
 
